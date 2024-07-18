9f3689fe3e0c3db0ab4ba5984be1d1390cbce5d7
lmaoggrofl <110363544+lmaoggrofl@users.noreply.github.com>
2024-07-18 11:49:07 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -2,12 +2,9 @@
 
 set -x
 #docker run -d -p 80:80 --name my-apache-php-app -v c:\\Users\\wilso\\Documents\\GitHub\\jenkins-phpunit-test\\src:/var/www/html php:7.2-apache
-# docker stop my-apache-php-app
-# docker rm my-apache-php-app
-#docker run -d -p 80:80 --name my-apache-php-app -v c:\\Users\\wilso\\Documents\\GitHub\\jenkins-phpunit-test\\src:/var/www/html php:7.2-apache
-docker run -d -p 80:80 --name my-apache-php-app -v /c/Users/wilso/Documents/GitHub/jenkins-phpunit-test/src:/var/www/html php:7.2-apache
-docker exec -it my-apache-php-app /bin/bash
-ls /var/www/html
+docker stop my-apache-php-app
+docker rm my-apache-php-app
+docker run -d -p 80:80 --name my-apache-php-app -v c:\\Users\\wilso\\Documents\\GitHub\\jenkins-phpunit-test\\src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
